8198ff71d2f1e61262a4d9de9f77a1dd387b62b3
Remo <calebremo99@gmail.com>
2026-05-26 01:43:24 -0400

    Added new system funtionality

--- astrometry.js
+++ astrometry.js
@@ -1,7 +1,7 @@
 import * as tri from './trilateration.js';
 import { updateSystemDropdown } from './main.js';
 
-const BACKEND = process.env.BACKEND_URL || 'http://localhost:4000';
+const BACKEND = process.env.BACKEND_URL;
 //const BACKEND = 'http://localhost:4000/';
 
 // Function to update system coordinates on the backend
