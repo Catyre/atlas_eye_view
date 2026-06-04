33af3370818c6691e7f4ba52aaa5c6b54f78252f
Remo <calebremo99@gmail.com>
2026-06-04 13:33:49 -0400

    Fixed new system panel for new default

--- main.js
+++ main.js
@@ -859,16 +859,22 @@ addSystemPanel.innerHTML = `
     <input type="text" id="new-hubtag" class="hud-input" placeholder="Hubtag" required>
     <input type="text" id="new-name" class="hud-input" placeholder="System Name" required>
     <input type="text" id="new-color" class="hud-input" placeholder="Star Color (yellow, red, green, blue, purple)" required>
-    <input type="number" step="any" id="new-a" class="hud-input" placeholder="Distance to [HUB44-A] Tashat" required>
-    <input type="number" step="any" id="new-b" class="hud-input" placeholder="Distance to [HUB45-A] Nasfiel-Nit" required>
-    <input type="number" step="any" id="new-c" class="hud-input" placeholder="Distance to [HUB17-A] Ochleac" required>
-    <input type="number" step="any" id="new-d" class="hud-input" placeholder="Distance to [HUB25-A] Snezhna" required>
+    <input type="number" step="any" id="new-a" class="hud-input" placeholder="Distance to [HUB12-416] Lion Shield" required>
+    <input type="number" step="any" id="new-b" class="hud-input" placeholder="Distance to [HUB1-74] Sun Tzu" required>
+    <input type="number" step="any" id="new-c" class="hud-input" placeholder="Distance to [HUB7-3FE] Aniwani" required>
+    <input type="number" step="any" id="new-d" class="hud-input" placeholder="Distance to [HUB22-406] Legods" required>
     <input type="number" step="any" id="new-e" class="hud-input" placeholder="Distance to [HUB21-1E] Sidusius" required>
     <button type="submit" class="hud-button submit">Transmit Coordinates</button>
   </form>
   <div id="add-status" style="margin-top: 12px; font-size: 12px; text-align: center;"></div>
 `;
 document.body.appendChild(addSystemPanel);
+// Euclid doesn't need fifth anchor
+const inputE = document.getElementById('new-e');
+inputE.style.display = 'none';
+inputE.required = false;
+inputE.value = ''; // Clear any leftover data
+
 
 toggleAddButton.addEventListener('click', (event) => {
   event.stopPropagation();
